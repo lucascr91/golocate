4ae28f33d4bc9e5416d28e18d207c5b78d2c4422
lucascr91 <lucas.ecomg@gmail.com>
2021-06-13 11:35:09 -0300

    add wildcard function

--- locate.go
+++ locate.go
@@ -7,11 +7,26 @@ import (
 )
 
 func main() {
-	home, _ := os.UserHomeDir()
-	os.Chdir(filepath.Join(home, ""))
-	matches, _:= filepath.Glob("*pdf")
-	for _, match := range matches {
-		fmt.Println(match)
+	var commands []string = os.Args
+	if len(commands) == 2 {
+		var pattern string = os.Args[1]
+		home, _ := os.UserHomeDir()
+		files, _ := os.ReadDir(home)
+		for _, match := range wildCard(home, files, pattern) {
+			fmt.Println(match)
+		}
 	}
+}
 
+func wildCard(home string, files []os.DirEntry, pattern string) []string {
+	var result []string
+	for _, file := range files {
+		if file.IsDir() {
+			matches, _ := filepath.Glob(filepath.Join(home, file.Name(), pattern))
+			for _, match := range matches {
+				result = append(result, match)
+			}
+		}
+	}
+	return result
 }
